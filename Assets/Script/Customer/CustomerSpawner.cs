138021c7e8585343c23d9adcad85007bb0e05d25
Axeoq <anthonysuryana2580@gmail.com>
2024-10-23 12:41:28 +0700

    Camera Fix II & Customer Spawn change

--- Assets/Script/Customer/CustomerSpawner.cs
+++ Assets/Script/Customer/CustomerSpawner.cs
@@ -22,13 +22,13 @@ public class CustomerSpawner : MonoBehaviour
         while(CustomerSum <= 10)
         {
             RadNum = Random.Range(1, 20); 
-            xPos = Random.Range(-5, -1); 
-            zPos = Random.Range(-13, -10);
+            xPos = Random.Range(13, 18); 
+            zPos = Random.Range(-7, -1);
             if(RadNum % 2 == 0)
             {
-                Instantiate(DineDasher, new Vector3 (xPos, 5, zPos), Quaternion.identity);
+                Instantiate(DineDasher, new Vector3 (xPos, 1, zPos), Quaternion.identity);
             } 
-            else Instantiate(ProperCustomer, new Vector3 (xPos, 5, zPos), Quaternion.identity);
+            else Instantiate(ProperCustomer, new Vector3 (xPos, 1, zPos), Quaternion.identity);
             yield return new WaitForSeconds(20); 
             CustomerSum += 1; 
         }
